8cd32c3228f41e7d97a6fbd0f17c1401d6c53641
Kalman Keri <kk@metamaya.org>
2017-08-22 09:09:21 +0200

    change double quotes to single quotes

--- ohm/build.js
+++ ohm/build.js
@@ -1,15 +1,15 @@
-﻿"use strict"
+﻿'use strict'
 
-const fs = require("fs");
-const path = require("path");
-const ohm = require("ohm-js");
+const fs = require('fs');
+const path = require('path');
+const ohm = require('ohm-js');
 
 const parserPath = path.join(__dirname, 'parser.ohm');
-const recipePath = path.join(__dirname, "recipe.js");
+const recipePath = path.join(__dirname, 'recipe.js');
 
 console.log('Compiling grammar...')
 let parser = ohm.grammar(fs.readFileSync(parserPath));
-console.log("Building parser recipe...");
+console.log('Building parser recipe...');
 let recipe = parser.toRecipe();
-fs.writeFileSync(recipePath, recipe, { encoding: "utf-8" });
-console.log("Done.");
+fs.writeFileSync(recipePath, recipe, { encoding: 'utf-8' });
+console.log('Done.');
