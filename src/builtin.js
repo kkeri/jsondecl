285185a46c81868405afc13c344eabacdf867113
Kalman Keri <kk@metamaya.org>
2017-08-26 01:08:34 +0200

    add builtin patterns

---
+++ src/builtin.js
@@ -0,0 +1,14 @@
+
+export const boolean = (x) => typeof x === 'boolean'
+
+export const number = (x) => typeof x === 'number'
+
+export const integer = (x) => number(x) && Math.floor(x) === x
+
+export const lt = (x, y) => number(x) && number(y) && x < y
+
+export const le = (x, y) => number(x) && number(y) && x <= y
+
+export const gt = (x, y) => number(x) && number(y) && x > y
+
+export const ge = (x, y) => number(x) && number(y) && x >= y
