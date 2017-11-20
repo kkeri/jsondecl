2ca5f33d482bbbc54323f8983403623fe8797f85
Kalman Keri <kk@metamaya.org>
2017-11-20 19:32:16 +0100

    Refactor closed() tests into a separate file

--- test/array-test.js
+++ test/array-test.js
@@ -62,44 +62,6 @@ test('array', t => {
   t.done()
 })
 
-test('closed array', t => {
-  t.match(compile('closed([])').test([]), true)
-  t.match(compile('closed([])').test([1]), false)
-
-  t.match(compile('closed([number])').test([1]), true)
-  t.match(compile('closed([number])').test([1, 2]), false)
-  t.match(compile('closed([number, string])').test([1, 2]), false)
-  t.match(compile('closed([number, string])').test([1, 'a']), true)
-  t.match(compile('closed([number, string])').test([1, 'a', 'b']), false)
-
-  t.match(compile('closed([number] | [string])').test([1]), true)
-  t.match(compile('closed([number] | [string])').test(['a']), true)
-  t.match(compile('closed([number] | [string])').test([1, 1]), false)
-  t.match(compile('closed([number] | [string])').test(['a', 1]), false)
-
-  t.match(compile('closed([string] | [string, number])').test(['a', 1]), true)
-  t.match(compile('closed([string, number] | [string])').test(['a', 1]), true)
-  t.match(compile('closed([string] & [string, number])').test(['a', 1]), true)
-  t.match(compile('closed([string, number] & [string])').test(['a', 1]), true)
-
-  t.match(compile('closed([string] | [string, number])').test(['a', 1, 2]), false)
-  t.match(compile('closed([string, number] | [string])').test(['a', 1, 2]), false)
-  t.match(compile('closed([string] & [string, number])').test(['a', 1, 2]), false)
-  t.match(compile('closed([string, number] & [string])').test(['a', 1, 2]), false)
-
-  t.match(compile('closed([number, any] & [any, string])').test([1, 'a']), true)
-  t.match(compile('closed([number, any] & [any, string])').test([1, 'a', 2]), false)
-
-  t.match(compile('closed([[number]])').test([[1]]), true)
-  t.match(compile('closed([[number]])').test([[1, 2]]), true)
-  t.match(compile('closed([[number]])').test([[1], 2]), false)
-
-  t.match(compile('closed(closed([number, string]))').test([1, 'a']), true)
-  t.match(compile('closed(closed([number, string]))').test([1, 'a', 'b']), false)
-
-  t.done()
-})
-
 test('array repetition', t => {
   t.match(compile('[]?').test([]), true)
   t.match(compile('[]*').test([]), true)
@@ -139,25 +101,6 @@ test('array repetition', t => {
   t.match(compile('[number, string]{2}').test([1, 'a', 2]), false)
   t.match(compile('[number, string]{2}').test([1, 'a', 2, 'b']), true)
   t.match(compile('[number, string]{2}').test([1, 'a', 2, 'b', 3]), true)
-
-  t.done()
-})
-
-test('closed array repetitions', t => {
-  t.match(compile('closed([number]?)').test([1, 2]), false)
-  t.match(compile('closed([number]{0,2})').test([1, 2, 3]), false)
-
-  t.match(compile('closed([number, string]*)').test([]), true)
-  t.match(compile('closed([number, string]*)').test([1]), false)
-  t.match(compile('closed([number, string]*)').test([1, 'a']), true)
-  t.match(compile('closed([number, string]*)').test([1, 'a', 2]), false)
-  t.match(compile('closed([number, string]*)').test([1, 'a', 2, 'b']), true)
-  t.match(compile('closed([number, string]*)').test([1, 2]), false)
-
-  t.match(compile('closed([number, string]{2})').test([1, 'a']), false)
-  t.match(compile('closed([number, string]{2})').test([1, 'a', 2]), false)
-  t.match(compile('closed([number, string]{2})').test([1, 'a', 2, 'b']), true)
-  t.match(compile('closed([number, string]{2})').test([1, 'a', 2, 'b', 3]), false)
 
   t.done()
 })
---
+++ test/closed-test.js
@@ -0,0 +1,189 @@
+'use strict'
+
+const test = require('tap').test
+const _compile = require('../lib/index').compile
+const model = require('../lib/model')
+const RuntimeError = require('../lib/diag').RuntimeError
+
+function compile (str) {
+  return _compile(str, {
+    filename: __filename
+  })
+}
+
+test('closed object', t => {
+  t.match(compile('closed({ "a": 1 })').test({}), false)
+  t.match(compile('closed({ "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 })').test({ b: 1 }), false)
+  t.match(compile('closed({ "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('closed({ "a": 1 })').test({ a: 1, b: 1 }), false)
+
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({}), false)
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({ a: 1 }), true)
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({ b: 2 }), true)
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({ a: 1, b: 2 }), true)
+  t.match(compile('closed({ "a": 1 } | { "b": 2 })').test({ a: 1, b: 2, c: 3 }), false)
+
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({}), false)
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({ a: 1 }), false)
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({ a: 1, b: 2 }), true)
+  t.match(compile('closed({ "a": 1 } & { "b": 2 })').test({ a: 1, b: 2, c: 3 }), false)
+
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({}), false)
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('closed({ "a": 1 } | { "a": 1 })').test({ a: 1, b: 2, c: 3 }), false)
+
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({}), false)
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('closed({ "a": 1 } & { "a": 1 })').test({ a: 1, b: 2, c: 3 }), false)
+
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({}), false)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ a: 1 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ b: 2 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ a: 1, b: 2 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ a: 1, b: 2, c: 3 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "b": 2 })').test({ b: 2, c: 3 }), false)
+
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({}), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ a: 1 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ b: 2 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ a: 1, b: 2, c: 3 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "b": 2 })').test({ b: 2, c: 3 }), false)
+
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({}), false)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ b: 2 }), false)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ a: 1, b: 2 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ a: 1, b: 2, c: 3 }), true)
+  t.match(compile('{ "a": 1 } | closed({ "a": 1 })').test({ b: 2, c: 3 }), false)
+
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({}), false)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ b: 2 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ a: 1, b: 2, c: 3 }), false)
+  t.match(compile('{ "a": 1 } & closed({ "a": 1 })').test({ b: 2, c: 3 }), false)
+
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({}), false)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ a: 1 }), true)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ b: 2 }), true)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ a: 1, b: 2, c: 3 }), false)
+  t.match(compile('closed({ "a": 1 }) | closed({ "b": 2 })').test({ b: 2, c: 3 }), false)
+
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({}), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ a: 1 }), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ a: 1, b: 2 }), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ a: 1, b: 2, c: 3 }), false)
+  t.match(compile('closed({ "a": 1 }) & closed({ "b": 2 })').test({ b: 2, c: 3 }), false)
+
+  t.done()
+})
+
+test('closed nested object', t => {
+  t.match(compile('closed({ "a": { "b": 2 } })').test({}), false)
+  t.match(compile('closed({ "a": { "b": 2 } })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": { "b": 2 } })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": { "b": 2 } })').test({ a: { b: 2 } }), true)
+  t.match(compile('closed({ "a": { "b": 2 } })').test({ a: { b: 2, c: 1 } }), true)
+  t.match(compile('closed({ "a": { "b": 2 } })').test({ a: 1, b: 1 }), false)
+
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({}), false)
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({ a: 'x' }), false)
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({ b: 2 }), false)
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({ a: { b: 2 } }), true)
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({ a: { b: 2, c: 1 } }), false)
+  t.match(compile('closed({ "a": closed({ "b": 2 }) })').test({ a: 1, b: 1 }), false)
+
+  t.done()
+})
+
+test('closed array', t => {
+  t.match(compile('closed([])').test([]), true)
+  t.match(compile('closed([])').test([1]), false)
+
+  t.match(compile('closed([number])').test([1]), true)
+  t.match(compile('closed([number])').test([1, 2]), false)
+  t.match(compile('closed([number, string])').test([1, 2]), false)
+  t.match(compile('closed([number, string])').test([1, 'a']), true)
+  t.match(compile('closed([number, string])').test([1, 'a', 'b']), false)
+
+  t.match(compile('closed([number] | [string])').test([1]), true)
+  t.match(compile('closed([number] | [string])').test(['a']), true)
+  t.match(compile('closed([number] | [string])').test([1, 1]), false)
+  t.match(compile('closed([number] | [string])').test(['a', 1]), false)
+
+  t.match(compile('closed([string] | [string, number])').test(['a', 1]), true)
+  t.match(compile('closed([string, number] | [string])').test(['a', 1]), true)
+  t.match(compile('closed([string] & [string, number])').test(['a', 1]), true)
+  t.match(compile('closed([string, number] & [string])').test(['a', 1]), true)
+
+  t.match(compile('closed([string] | [string, number])').test(['a', 1, 2]), false)
+  t.match(compile('closed([string, number] | [string])').test(['a', 1, 2]), false)
+  t.match(compile('closed([string] & [string, number])').test(['a', 1, 2]), false)
+  t.match(compile('closed([string, number] & [string])').test(['a', 1, 2]), false)
+
+  t.match(compile('closed([number, any] & [any, string])').test([1, 'a']), true)
+  t.match(compile('closed([number, any] & [any, string])').test([1, 'a', 2]), false)
+
+  t.match(compile('closed([[number]])').test([[1]]), true)
+  t.match(compile('closed([[number]])').test([[1, 2]]), true)
+  t.match(compile('closed([[number]])').test([[1], 2]), false)
+
+  t.match(compile('closed(closed([number, string]))').test([1, 'a']), true)
+  t.match(compile('closed(closed([number, string]))').test([1, 'a', 'b']), false)
+
+  t.done()
+})
+
+test('closed array repetitions', t => {
+  t.match(compile('closed([number]?)').test([1, 2]), false)
+  t.match(compile('closed([number]{0,2})').test([1, 2, 3]), false)
+
+  t.match(compile('closed([number, string]*)').test([]), true)
+  t.match(compile('closed([number, string]*)').test([1]), false)
+  t.match(compile('closed([number, string]*)').test([1, 'a']), true)
+  t.match(compile('closed([number, string]*)').test([1, 'a', 2]), false)
+  t.match(compile('closed([number, string]*)').test([1, 'a', 2, 'b']), true)
+  t.match(compile('closed([number, string]*)').test([1, 2]), false)
+
+  t.match(compile('closed([number, string]{2})').test([1, 'a']), false)
+  t.match(compile('closed([number, string]{2})').test([1, 'a', 2]), false)
+  t.match(compile('closed([number, string]{2})').test([1, 'a', 2, 'b']), true)
+  t.match(compile('closed([number, string]{2})').test([1, 'a', 2, 'b', 3]), false)
+
+  t.done()
+})
+
+test('closed combinations', t => {
+  t.throws(function () { compile('closed()').test({}) }, RuntimeError)
+
+  t.match(compile('closed(1)').test({}), false)
+  t.match(compile('closed(1)').test(1), true)
+
+  t.match(compile('closed(1 | { "a": 1 })').test(1), true)
+  t.match(compile('closed(1 | { "a": 1 })').test({}), false)
+  t.match(compile('closed(1 | { "a": 1 })').test({ a: 'x' }), false)
+  t.match(compile('closed(1 | { "a": 1 })').test({ b: 1 }), false)
+  t.match(compile('closed(1 | { "a": 1 })').test({ a: 1 }), true)
+  t.match(compile('closed(1 | { "a": 1 })').test({ a: 1, b: 1 }), false)
+
+  t.done()
+})
